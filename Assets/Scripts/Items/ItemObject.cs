a213a69ec0e6d40ab03ff74eb7b38902ae49d378
EcchiClone <aa01092308481@gmail.com>
2024-02-05 06:24:38 +0900

    Feature: 아이템과 상호작용

---
+++ Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData item;
+
+    public string GetInteractPrompt()
+    {
+        return string.Format("Pickup {0}", item.displayName); // 프롬프트 받아오도록
+    }
+
+    public void OnInteract()
+    {
+        Destroy(gameObject); // 상호작용 했을 때 어떻게 동작할지
+    }
+}
